f56d0953198c651664fa9d5703308e88d644fbdc
Attila Starkenius <AttilaStarkenius@users.noreply.github.com>
2021-06-11 12:07:57 +0200

    Add files via upload

---
+++ CharactersInResultViewModel.cs
@@ -0,0 +1,39 @@
+﻿using httpdeveloper.marvel.comdocsAPIMVCnewestversion.Models;
+using Marvel.Api.Model.Lists;
+using Marvel.Api.Model.Summaries;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace httpdeveloper.marvel.comdocsAPIMVCnewestversion.Models
+{
+    public class CharactersInResultViewModel
+    {
+
+        public StoryList storyList { get; set; }
+
+
+        public RestRequest request { get; set; }
+        public List<RestRequest> requestList { get; set; }
+        public string StoryID { get; set; }
+        public CharacterList Characters { get; set; }
+
+        public List<ResultViewModel> CharactersInTheStory;
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        //public static implicit operator CharactersInResultViewModel(RestRequest v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public static explicit operator RestRequest(CharactersInResultViewModel v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
